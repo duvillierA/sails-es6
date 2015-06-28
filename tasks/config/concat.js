2b4139aa38dc9c53f7a0ed7e3b15d91fd532d1cc
Duvillier Antoine <duvillierantoine@gmail.com>
2015-06-28 12:40:10 -0700

    [tasks] prepare transition to gulp

--- tasks/config/concat.js
+++ tasks/config/concat.js
@@ -13,14 +13,14 @@
 module.exports = function(grunt) {
 
 	grunt.config.set('concat', {
-		js: {
-			src: require('../pipeline').jsFilesToInject,
-			dest: '.tmp/public/concat/production.js'
-		},
-		css: {
-			src: require('../pipeline').cssFilesToInject,
-			dest: '.tmp/public/concat/production.css'
-		}
+		// js: {
+		// 	src: require('../pipeline').jsFilesToInject,
+		// 	dest: '.tmp/public/concat/production.js'
+		// },
+		// css: {
+		// 	src: require('../pipeline').cssFilesToInject,
+		// 	dest: '.tmp/public/concat/production.css'
+		// }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
